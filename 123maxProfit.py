976523db51ed4da09c1ae294c0ca7fd873b65794
Niu <qinzhouhit@gmail.com>
2020-05-08 02:19:56 -0400

    sols

---
+++ 123maxProfit.py
@@ -0,0 +1,72 @@
+'''
+keys: DP
+Solutions:
+Similar:
+T:
+S:
+'''
+from typing import List
+
+class Solution:
+
+    # TODO: one-pass simulation
+    # S: O(1), four costs/ profits followed by four transactions
+    def maxProfit1(self, prices: List[int]) -> int:
+        t1_cost, t2_cost = float('inf'), float('inf')
+        t1_profit, t2_profit = 0, 0
+
+        for price in prices:
+            # the maximum profit if only one transaction is allowed
+            t1_cost = min(t1_cost, price)
+            t1_profit = max(t1_profit, price - t1_cost)
+            # reinvest the gained profit in the second transaction
+            # taking into account the profit gained from the previous transaction #1
+            # consider this as the cost of reinvestment
+            # i.e., the more the t1_profit, the lower the t2_cost
+            # then the higher the t2_profit
+            t2_cost = min(t2_cost, price - t1_profit)
+            t2_profit = max(t2_profit, price - t2_cost)
+
+        return t2_profit
+
+
+
+
+    # TODO: bidirectional dp, divide and conquer
+    # T: O(N) for length of input sequence
+    # S: O(N) for the two arrays we keep
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) <= 1:
+            return 0
+
+        left_min = prices[0]
+        right_max = prices[-1]
+
+        length = len(prices)
+        # those left_profits and right_profits are for one transaction gain
+        # left_profits[i] for prices[0:i+1]
+        left_profits = [0] * length
+        # pad the right DP array with an additional zero for convenience.
+        # right_profits[i] for prices[i:]
+        # Have to pad extra 0 for case that only one transaction happened
+        # i.e., left_profits[N-1] and empty right subsequence
+        right_profits = [0] * (length + 1)
+
+        # construct the bidirectional DP array
+        for l in range(1, length):
+            left_profits[l] = max(left_profits[l-1], prices[l] - left_min)
+            left_min = min(left_min, prices[l])
+
+            r = length - 1 - l
+            right_profits[r] = max(right_profits[r+1], right_max - prices[r])
+            right_max = max(right_max, prices[r])
+
+        max_profit = 0
+        for i in range(0, length):
+            max_profit = max(max_profit, left_profits[i] + right_profits[i+1])
+
+        return max_profit
+
+
+sol = Solution()
+print (sol.maxProfit([1,2,3,4,5]))
---
+++ 128longestConsecutive.py
@@ -0,0 +1,79 @@
+'''
+keys: backtracking and DP
+Solutions:
+Similar:
+T:
+S:
+'''
+from typing import List
+
+class Solution:
+    # TODO: HashSet and Intelligent Sequence Building
+    # based on the brutal force, use set
+    # T: O(n+n), O(n) for for loop and O(n) for while loop
+    # S: O(n) for the set
+    def longestConsecutive2(self, nums: List[int]) -> int:
+        longest_streak = 0
+        num_set = set(nums)
+
+        for num in num_set:
+            # attempt to build sequences from numbers that are not already part of a longer sequence.
+            # first ensuring that the number that would immediately precede the current number in a sequence is not present
+            if num - 1 not in num_set: # genius step...
+                current_num = num
+                current_streak = 1
+
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
+
+                longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
+
+
+    # TODO: sorting
+    # T: O(nlogn)
+    # S: O(1) or O(n) for the sorted array
+    def longestConsecutive1(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        nums.sort()
+        longest_streak = 1
+        current_streak = 1
+
+        for i in range(1, len(nums)):
+            # if they are equal, we just continue since equality does not
+            # affect the streak, e.g., [-1, -1, 0, 1], just move to the 2nd -1
+            if nums[i] != nums[i-1]:
+                if nums[i] == nums[i-1]+1:
+                    current_streak += 1
+                else:
+                    longest_streak = max(longest_streak, current_streak)
+                    current_streak = 1
+        # becuase the current_streak (w/ last element) may be the longest
+        return max(longest_streak, current_streak)
+        # return longest_streak
+
+    # TODO: brutal force
+    # T: O(n^3): outer loop//current_num incremented by 1//O(n) lookup for "in" the array
+    # S: O(1) for those integer variables
+    def longestConsecutive(self, nums: List[int]) -> int:
+        longest_streak = 0
+
+        for num in nums:
+            current_num = num
+            current_streak = 1
+
+            while current_num + 1 in nums:
+                current_num += 1
+                current_streak += 1
+
+            longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
+
+
+sol = Solution()
+print (sol.longestConsecutive2([9,1,4,7,3,-1,0,5,8,-1,6]))
