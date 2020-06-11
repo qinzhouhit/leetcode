13d48889669fdd860b0aafcf358db511db4702bf
Zhou Qin <qinzhouhit@gmail.com>
2020-06-10 23:41:43 -0400

    sol

---
+++ 547findCircleNum.py
@@ -0,0 +1,1 @@
+'''keys: transform matrix into graph, then find connected componentsSolutions:Similar: T:S:'''from typing import Listclass Solution:    def findCircleNum(self, M: List[List[int]]) -> int:        
---
+++ 688knightProbability.py
@@ -0,0 +1,1 @@
+'''keys: Solutions:Similar: T:S:'''from typing import Listclass Solution:    def knightProbability(self, N: int, K: int, r: int, c: int) -> float:        pass
