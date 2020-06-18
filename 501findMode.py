7b97c60b97e62c46c8da6c93bd6b9cef9abbd73f
Zhou Qin <qinzhouhit@gmail.com>
2020-06-18 00:33:38 -0400

    sols

---
+++ 501findMode.py
@@ -0,0 +1,1 @@
+'''keys: Solutions:Similar: T:S:'''from typing import List# Definition for a binary tree node.class TreeNode:    def __init__(self, val=0, left=None, right=None):        self.val = val        self.left = left        self.right = right        class Solution:    def findMode(self, root: TreeNode) -> List[int]:                
