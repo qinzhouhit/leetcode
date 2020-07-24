e1f577fca638cd97a4bbe9c6be278323f58be8df
okapix <qinzhouhit@gmail.com>
2020-07-23 23:38:40 -0400

    all

--- 560subarraySum.py
+++ 560subarraySum.py
@@ -1,1 +1,71 @@
-'''keys: Solutions:Similar:340T:S:'''from typing import Listclass Solution:    def subarraySum(self, nums: List[int], k: int) -> int:        
+'''
+keys: 
+Solutions:
+Similar:340
+T:
+S:
+'''
+from typing import List
+
+
+class Solution:
+    # shorter, prefix sum
+    # whenever the sums has increased by a value of k, 
+    # we've found a subarray of sums=k.
+    # https://leetcode.com/problems/subarray-sum-equals-k/discuss/341399/Python-clear-explanation-with-code-and-example
+    def subarraySum2(self, nums: List[int], k: int) -> int:
+        count = 0
+        sums = 0
+        d = dict()
+        d[0] = 1
+        
+        for i in range(len(nums)):
+            sums += nums[i]
+            count += d.get(sums-k,0)
+            d[sums] = d.get(sums,0) + 1
+        return count
+    
+    # hashmap
+    # O(n) for S and T
+    '''
+    // Sliding window -- No, contains negative number
+        // hashmap + preSum
+        /*
+            1. Hashmap<sum[0,i - 1], frequency>
+            2. sum[i, j] = sum[0, j] - sum[0, i - 1]    --> sum[0, i - 1] = sum[0, j] - sum[i, j]
+                   k           sum      hashmap-key     -->  hashmap-key  =  sum - k
+            3. now, we have k and sum.  
+                  As long as we can find a sum[0, i - 1], we then get a valid subarray
+                 which is as long as we have the hashmap-key,  we then get a valid subarray
+            4. Why don't map.put(sum[0, i - 1], 1) every time ?
+                  if all numbers are positive, this is fine
+                  if there exists negative number, there could be preSum frequency > 1
+        */'''
+    def subarraySum1(self, nums: List[int], k: int) -> int:
+        ct = 0; sums = 0
+        hashmap = {}
+        hashmap[0] = 1
+        
+        for i in range(len(nums)):
+            sums += nums[i]
+            if sums - k in hashmap:
+                ct += hashmap[sums - k]
+            hashmap[sums] = hashmap.get(sums, 0) + 1
+        return ct
+        
+        
+    
+    # LTE
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        ct = 0
+        for i in range(len(nums)):
+            tmp_sum = 0
+            for j in range(i, len(nums)):
+                tmp_sum += nums[j]
+                if tmp_sum == k:
+                    ct += 1
+        return ct
+    
+    
+sol = Solution()
+sol.subarraySum2([1,2,1,3], 3)
--- 802eventualSafeNodes.py
+++ 802eventualSafeNodes.py
@@ -1,1 +1,72 @@
-'''keys: Solutions:Similar: T:S:'''from typing import Listclass Solution:    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:                        
+'''
+keys: https://leetcode.com/problems/find-eventual-safe-states/discuss/119827/20-line-Python-concise-sol-by-removing-0-out-degree-nodes
+Solutions:
+Similar: 
+T:
+S:
+'''
+from typing import List
+import collections
+
+class Solution:
+
+    # reverse edges
+    # O(N+E) for T and O(N) for S
+    def eventualSafeNodes1(self, graph: List[List[int]]) -> List[int]:
+        N = len(graph)
+        safe = [False] * N
+
+        graph = map(set, graph)
+        rgraph = [set() for _ in range(N)]
+        q = collections.deque() # for terminate nodes
+
+        for i, js in enumerate(graph):
+            if not js:
+                q.append(i)
+            for j in js:
+                rgraph[j].add(i) # recording edges from i to j
+
+        while q:
+            j = q.popleft()
+            safe[j] = True
+            for i in rgraph[j]: # i: nodes into terminate nodes
+                graph[i].remove(j)
+                if len(graph[i]) == 0:
+                    q.append(i)
+
+        return [i for i, v in enumerate(safe) if v]
+        
+        
+    
+    # T: O(nlogn), because of the sort
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        '''
+        Find nodes with out degree 0, they are terminal nodes, we 
+        remove them from graph and they are added to result
+        For nodes who are connected terminal nodes, since terminal 
+        nodes are removed, we decrease in-nodes' out degree by 1 and 
+        if its out degree equals to 0, it become new terminal nodes
+        Repeat 2 until no terminal nodes can be found.
+
+        '''
+        n = len(graph)
+        out_degree = [0]*n
+        in_nodes = collections.defaultdict(list)
+        queue = []
+        for i in range(n):
+            out_degree[i] = len(graph[i])
+            if out_degree[i] == 0:
+                queue.append(i) # terminate nodes
+            for j in graph[i]:
+                # edges from i (starting) to j (destination)
+                in_nodes[j].append(i) 
+        for term_node in queue: # terminate_node
+            for in_node in in_nodes[term_node]:
+                out_degree[in_node] -= 1
+                if out_degree[in_node] == 0:
+                    queue.append(in_node)
+        return sorted(queue)
+                
+            
+        
+        
