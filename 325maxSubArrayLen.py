6e4a673f8ef13126fbc062610e75dcd050c43bf7
Zhou Qin <qinzhouhit@gmail.com>
2020-11-20 12:23:07 -0500

    sols

--- 325maxSubArrayLen.py
+++ 325maxSubArrayLen.py
@@ -13,6 +13,25 @@ class Solution:
     @param k: a target value
     @return: the maximum length of a subarray that sums to k
     """
+    
+    def maxSubArrayLen1(self, nums: List[int], k: int) -> int:
+        res, acc = 0, 0
+        # This way, whenever acc = 0 in your subarray, the value at
+        # mp[0] remains -1 instead of being changed to the current 
+        # index, i. You want to include consecutive values in your 
+        # subarray that total to 0 in order to get the longest 
+        # subarray count that sums up to k.
+        h = {0:-1} 
+        
+        for idx, val in enumerate(nums):
+            acc += val
+            if acc not in h:
+                h[acc] = idx
+            if acc - k in h:
+                res = max(res, idx - h[acc-k])
+        # print (h)
+        return res
+    
     # O(n) for S and T
     def maxSubArrayLen(self, nums: List[int], k: int) -> int:
         # Write your code here
