5e1ec95b5f168e4427039e065341de6d63417fe3
Zhou Qin <qinzhouhit@gmail.com>
2020-11-22 18:06:43 -0500

    sols

--- 101isSymmetric.py
+++ 101isSymmetric.py
@@ -5,8 +5,14 @@ Similar:
 T:
 S:
 '''
-
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
 class Solution:
+    # recursive
     def isSymmetric(self, root: TreeNode) -> bool:
         if root is None:
             return True
@@ -20,12 +26,14 @@ class Solution:
             return False
 
         if leftNode.val == rightNode.val:
+            # return self.helper(node1.left, node2.right) and self.helper(node1.right, node2.left)
             outer = self.helper(leftNode.left, rightNode.right)
             inner = self.helper(leftNode.right, rightNode.left)
             return outer and inner
         else:
             return False
 
+    # iterative
     def isSymmetric1(self, root):
         if root is None:
             return True
